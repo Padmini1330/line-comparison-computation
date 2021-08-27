92c28843b93645a37536a008bdfff51080d8a06e
padmini v <padmini.v@perfios.com>
2021-08-27 07:47:26 +0530

    ADDED: method to calculate the length of line

--- src/com/bridgelabz/linecomparisoncomputation/LineComparisonComputation.java
+++ src/com/bridgelabz/linecomparisoncomputation/LineComparisonComputation.java
@@ -1,12 +1,31 @@
 package com.bridgelabz.linecomparisoncomputation;
 
+import java.util.Scanner;
 public class LineComparisonComputation 
 {
+	
+	public void lineLength(int x1,int y1,int x2,int y2) 
+		{
+			double length=Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+			System.out.println("Length of Line is " + length);
+		}
+		
 
-	public static void main(String[] args) 
-	{
-		System.out.println("***WELCOME TO LINE COMPARISON COMPUTATION***");
-
-	}
+		public static void main(String[] args) 
+		{
+			LineComparisonComputation ob=new LineComparisonComputation();
+			Scanner scannerObject=new Scanner(System.in);
+			System.out.println("Welcome to Line Comparison Computation Program ");
+			System.out.println("Enter the coordinates (x1, y1, x2 and y2) of line ");
+			int x1=scannerObject.nextInt();
+			int y1=scannerObject.nextInt();
+			int x2=scannerObject.nextInt();
+			int y2=scannerObject.nextInt();
+			ob.lineLength(x1, y1, x2, y2);
+			
+		}
+			
 
 }
+
+
