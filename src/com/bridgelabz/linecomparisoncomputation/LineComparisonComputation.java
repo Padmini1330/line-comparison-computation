66e0cfad391f712efa693c57e70026a1eddf4cb8
padmini v <padmini.v@perfios.com>
2021-08-27 07:43:36 +0530

    ADDED: Welcome message

---
+++ src/com/bridgelabz/linecomparisoncomputation/LineComparisonComputation.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.linecomparisoncomputation;
+
+public class LineComparisonComputation 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("***WELCOME TO LINE COMPARISON COMPUTATION***");
+
+	}
+
+}
