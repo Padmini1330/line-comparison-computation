52ecabf67a2261d4d1b718eb2f5ce9b97c3a4baa
padmini v <padmini.v@perfios.com>
2021-08-30 20:45:48 +0530

    ADDED: Point class to keep track of line coordinates

---
+++ src/com/bridgelabz/linecomparisoncomputation/Point.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.linecomparisoncomputation;
+
+public class Point 
+{
+	private int xCoordinate;
+	private int yCoordinate;
+
+	public int getXCoordinate() 
+	{
+		return xCoordinate;
+	}
+	
+	
+	public int getYCoordinate() 
+	{
+		return yCoordinate;
+	}
+
+	
+	public void setXCoordinate(int xCoordinate) 
+	{
+		this.xCoordinate = xCoordinate;
+	}
+	
+
+	public void setYCoordinate(int yCoordinate) 
+	{
+		this.yCoordinate = yCoordinate;
+	}
+}
